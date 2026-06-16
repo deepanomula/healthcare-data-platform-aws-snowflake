ca42c554e4a6305dec62c01d554cc38e96e2e4d3
Deepa <deepa.nomula@gmail.com>
2026-06-16 15:22:12 -0500

    cd: format account_name as locator.region for strict v1.0 driver compliance

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,17 +23,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# 💡 Use the new v1.0 argument structures to satisfy the schema compiler
+# 💡 Clean, strict v1.0 parameters
 variable "snowflake_organization_name" { type = string }
 variable "snowflake_account_name"      { type = string }
-variable "snowflake_host"              { type = string }
 variable "snowflake_user"              { type = string }
 variable "snowflake_password"          { type = string }
 
 provider "snowflake" {
   organization_name = var.snowflake_organization_name
   account_name      = var.snowflake_account_name
-  host              = var.snowflake_host
   user              = var.snowflake_user
   password          = var.snowflake_password
 }
