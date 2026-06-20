5738f0c17a4eda9898384a8a5ac6140ab24556a2
Deepa <deepa.nomula@gmail.com>
2026-06-19 19:52:50 -0500

    fix: sync state for deleted lambda and add snowflake stage preview feature

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -54,5 +54,6 @@ provider "snowflake" {
   user              = var.SNOWFLAKE_USER
   authenticator     = "SNOWFLAKE"
   password          = var.SNOWFLAKE_PASSWORD
-  preview_features_enabled = ["snowflake_storage_integration_resource"]
+  preview_features_enabled = ["snowflake_storage_integration_resource", 
+    "snowflake_stage_resource"]
 }
